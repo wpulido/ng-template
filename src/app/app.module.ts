5e311842c515821a4b2f51480883faced3d18826
Wistre Pulido <jstr.pulido9@gmail.com>
2019-10-21 23:32:47 -0400

    rutas añadidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, PortfolioComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, PortfolioComponent, AboutComponent, ItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
